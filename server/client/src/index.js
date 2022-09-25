c7be0fba3e15411468aad053af20aba00ce373d1
ruhulsinbad <ruhulsinbad@gmail.com>
2022-09-25 15:45:47 +0600

    check

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://sinugql.herokuapp.com/graphql",
   cache: new InMemoryCache(),
   headers: {
     authorization: localStorage.getItem("token") || "",
